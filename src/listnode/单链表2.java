1ccc6fe39b3022401e82ce9f9ca7046577627169
gmy1234 <1508594767@qq.com>
2022-02-28 23:31:16 +0800

    leetcode：2，两链表之和

--- src/listnode/单链表2.java
+++ src/listnode/单链表2.java
@@ -1,5 +1,7 @@
 package listnode;
 
+import java.sql.SQLOutput;
+
 /**
  * @version 1.0
  * @Description: gmy
@@ -73,6 +75,44 @@ public class 单链表2 {
         }
 
         return dummy.next;
+    }
+
+    /**
+     * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，
+     * 并且每个节点只能存储一位数字。
+     *
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 结果
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 
 
